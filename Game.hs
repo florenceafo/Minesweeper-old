17ca3fad5690495765040330f17e57c268e08e15
florenceafo <florence.afolabi@gmail.com>
2021-02-01 19:13:22 +0100

    Feature: creates an all blank board of a given size

---
+++ Game.hs
@@ -0,0 +1,76 @@
+module Game where
+
+import Test.QuickCheck
+import Data.List
+
+import System.Random
+
+
+data Grid = Grid [Row]
+  deriving (Show, Eq, Read)
+type Row = [Cell] 
+data Cell = Blank AdjBombs Clicked | Bomb
+  deriving (Show, Eq, Read)
+type AdjBombs = Int -- number of bombs to above, below or either side
+type Flag = Bool -- has the user flagged the cell (right click)
+type Clicked = Bool -- has the user clicked on the cell (left click)
+
+  
+-- turns the Grid into a nested list
+rows :: Grid -> [Row]
+rows (Grid g) = g
+
+-- example Grids for testing
+ex1, ex2, ex3, ex4 :: Grid
+ex1 = Grid  [
+              [Blank 0 False , Blank 1 False , Blank 0 False ],
+              [Blank 2 False , Bomb, Blank 3 False ],
+              [Blank 0 False , Blank 4 False , Blank 0 False ]
+            ]
+
+ex2 =  Grid [
+  [Blank 0 True,Blank 0 True,Bomb,Blank 0 True,Blank 0 True,Blank 0 True,Blank 0 True,Bomb,Blank 0 True],
+  [Bomb,Blank 0 True,Blank 0 True,Blank 0 True,Blank 0 True,Bomb,Blank 0 True,Blank 0 True,Blank 0 True],
+  [Bomb,Blank 0 True,Bomb,Blank 0 True,Blank 0 True,Bomb,Blank 0 True,Blank 0 True,Blank 0 True],
+  [Blank 0 True,Blank 0 True,Blank 0 True,Blank 0 True,Blank 0 True,Blank 0 True,Blank 0 True,Blank 0 True,Blank 0 True],
+  [Bomb,Bomb,Blank 0 True,Blank 0 True,Blank 0 True,Bomb,Blank 0 True,Blank 0 True,Blank 0 True],
+  [Blank 0 True,Bomb,Blank 0 True,Blank 0 True,Blank 0 True,Blank 0 True,Blank 0 True,Blank 0 True,Blank 0 True],
+  [Blank 0 True,Bomb,Blank 0 True,Blank 0 True,Bomb,Blank 0 True,Bomb,Blank 0 True,Blank 0 True],
+  [Blank 0 True,Bomb,Blank 0 True,Blank 0 True,Blank 0 True,Blank 0 True,Blank 0 True,Blank 0 True,Bomb],
+  [Blank 0 True,Blank 0 True,Blank 0 True,Blank 0 True,Bomb,Bomb,Bomb,Bomb,Blank 0 True]
+  ]
+
+ex3 =  Grid [
+  [Blank 0 True,Blank 0 True,Blank 0 True,Blank 0 True,Blank 0 True,Blank 0 True,Blank 0 True,Blank 0 True,Blank 0 True],
+  [Blank 0 True,Blank 0 True,Blank 0 True,Blank 0 True,Blank 0 True,Blank 0 True,Blank 0 True,Blank 0 True,Blank 0 True],
+  [Blank 0 True,Blank 0 True,Blank 0 True,Blank 0 True,Blank 0 True,Blank 0 True,Blank 0 True,Blank 0 True,Blank 0 True],
+  [Blank 0 True,Blank 0 True,Blank 0 True,Blank 1 True,Blank 0 True,Blank 0 True,Blank 0 True,Blank 0 True,Blank 0 True],
+  [Blank 0 True,Blank 0 True,Blank 1 True,Bomb,Blank 1 True,Blank 0 True,Blank 0 True,Blank 0 True,Blank 0 True],
+  [Blank 0 True,Blank 0 True,Blank 0 True,Blank 1 True,Blank 0 True,Blank 0 True,Blank 0 True,Blank 0 True,Blank 0 True],
+  [Blank 0 True,Blank 0 True,Blank 0 True,Blank 0 True,Blank 0 True,Blank 0 True,Blank 0 True,Blank 0 True,Blank 0 True],
+  [Blank 0 True,Blank 0 True,Blank 0 True,Blank 0 True,Blank 0 True,Blank 0 True,Blank 0 True,Blank 0 True,Blank 0 True],
+  [Blank 0 True,Blank 0 True,Blank 0 True,Blank 0 True,Blank 0 True,Blank 0 True,Blank 0 True,Blank 0 True,Blank 0 True]
+  ]
+
+ex4 = Grid  [
+              [Blank 0 False , Blank 0 False , Blank 0 False ],
+              [Blank 0 False , Bomb, Blank 0 False ],
+              [Blank 0 False , Blank 0 False , Blank 0 False ]
+            ]
+
+
+-- creates a blank Grid with the height and width of the given Integer
+-- Implements a maximum size of 10 x 10 and a minimum size of 5 x 5
+allBlankGrid :: Int -> Grid
+allBlankGrid i = Grid [[Blank 0 False | _ <- [1..j]] | _ <- [1..j]]
+  where 
+    j 
+     | i < 5 = 5
+     | i > 10 = 10
+     | otherwise = i
+
+-- checks if the blank grid generated contains all blanks, 
+-- with no bombs or adjacency numbers
+prop_allBlankGrid :: Grid -> Bool
+prop_allBlankGrid grid = all (== Blank 0 False) $ concat $ rows grid
+
